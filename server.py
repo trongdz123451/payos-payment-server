f8899864dd48dd8ca3340974bbd88f29b96929a1
trongdz123451 <trongvu20006afj@gmail.com>
2025-05-26 14:26:28 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,42 +2,42 @@ import json
 import os
 import random
 from dotenv import load_dotenv
-from payos import PaymentData, ItemData, PayOS
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-
 load_dotenv()
 
-# Gọi đúng tên biến, không đè class
-payos = PayOS(
-    client_id=os.environ.get('PAYOS_CLIENT_ID'),
-    api_key=os.environ.get('PAYOS_API_KEY'),
-    checksum_key=os.environ.get('PAYOS_CHECKSUM_KEY')
-)
+from payos import PaymentData, ItemData, PayOS
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
 
-app = Flask(__name__, static_folder='public', static_url_path='', template_folder='public')
+PayOS = PayOS(client_id= os.environ.get('PAYOS_CLIENT_ID'),api_key= os.environ.get('PAYOS_API_KEY'), checksum_key= os.environ.get('PAYOS_CHECKSUM_KEY'))
+
+app = Flask(__name__, static_folder='public',
+            static_url_path='', template_folder='public')
 CORS(app)
 
 @app.route('/create_payment_link', methods=['POST'])
 def create_payment():
     domain = "http://127.0.0.1:5000"
     try:
-        paymentData = PaymentData(
-            orderCode=random.randint(1000, 99999),
-            amount=69000,
-            description="Mở khoá ebook",
-            cancelUrl=f"{domain}/cancel.html",
-            returnUrl="https://unlockebookcoaytunguyen.netlify.app/",
-            items=[
-                ItemData(name="Mở khoá ebook", quantity=1, price=69000)
-            ]
+       paymentData = PaymentData(
+    orderCode=random.randint(1000, 99999),
+    amount=69000,
+    description="Mở khoá ebook",
+    cancelUrl=f"{domain}/cancel.html",
+    returnUrl="{https://unlockebookcoaytunguyen.netlify.app/}",
+    items=[
+        ItemData(
+            name="Mở khoá ebook",
+            quantity=1,
+            price=69000
         )
-        payosCreatePayment = payos.createPaymentLink(paymentData)
-        return jsonify(payosCreatePayment.to_json())
+    ]
+)
+       payosCreatePayment = PayOS.createPaymentLink(paymentData)
+       return jsonify(payosCreatePayment.to_json())
     except Exception as e:
-        import traceback
-        print(traceback.format_exc())
-        return jsonify(error=str(e)), 403
-
+     import traceback
+     print(traceback.format_exc())  # In ra lỗi chi tiết trong terminal
+     return jsonify(error=str(e)), 403
+    
 if __name__ == "__main__":
-    app.run(port=4242, debug=True)
+   app.run(port=4242, debug=True)
